f2358631a3291bed49c903c41cdac79bb8528bb2
BHARLAPALLY APPOORVA <bharlapallyappoorvabharlapally@gmail.com>
2025-08-21 21:44:03 +0530

    commit

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 a=int(input("Enter the first number:"))
 b=int(input("Enter the second number:"))
-addition=a+b
-subtraction=a-b
-multiplication=a*b
-division=a/b
+addition=a + b
+subtraction=a - b
+multiplication=a * b
+division=a / b
 print("Addition :",addition)
 print("Subtraction :",subtraction)
 print("Multiplication :",multiplication)
