723163106c83ce52f4ff7360b0308a8be9b10d46
BHARLAPALLY APPOORVA <bharlapallyappoorvabharlapally@gmail.com>
2025-08-24 21:20:08 +0530

    commit

---
+++ read.file.py
@@ -0,0 +1,9 @@
+try:
+    with open("sample.txt","r") as f:
+        print("Reading file content:")
+        line_num=1
+        for line in f:
+            print(f"line {line_num}: {line.strip()}")
+            line_num+=1
+except FileNotFoundError:
+    print("Error: the file 'sample.txt' was not found.")
